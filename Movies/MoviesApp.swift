6732cf4c4c42c594512b2c7e69e3d34e411cfeb0
Ilya Sirosh <sirosh.ilya@gmail.com>
2021-02-04 11:57:15 +0200

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Illya Sirosh on 04.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
